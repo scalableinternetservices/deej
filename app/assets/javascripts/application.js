0b6505122e0c9cf880d6b7ab1219308bc55006e9
Nathaniel Chu <nathanielchu4@gmail.com>
2016-11-02 22:08:01 +0000

    Added playlist from Deezer to Playlist tab on Profile page. However, I could not play sound, maybe because I don't have a Deezer account?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,11 @@ for (i = 0; i < tablinks.length; i++) {
 document.getElementById(cityName).style.display = "block";
 evt.currentTarget.className += " active";
 }
+
+function getPlaylist(d, s, id) { 
+	var js, djs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return; 
+	js = d.createElement(s); js.id = id; 
+	js.src = "https://cdns-files.dzcdn.net/js/widget/loader.js"; 
+	 djs.parentNode.insertBefore(js, djs);
+}
