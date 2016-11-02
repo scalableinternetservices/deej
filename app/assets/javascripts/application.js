4b589c72811e51687e9ea911b2c12b82ec99503a
Nathaniel Chu <nathanielchu4@gmail.com>
2016-11-02 21:55:46 +0000

    Added tabs to Profile Page.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,17 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+function openTab(evt, cityName) {
+var i, tabcontent, tablinks;
+tabcontent = document.getElementsByClassName("tabcontent");
+for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+}
+tablinks = document.getElementsByClassName("tablinks");
+for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+}
+document.getElementById(cityName).style.display = "block";
+evt.currentTarget.className += " active";
+}
