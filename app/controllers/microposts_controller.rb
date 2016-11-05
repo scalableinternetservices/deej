e4c27e50f5ddc33215bb112dc66d8bc2fd0d3a03
David scheibe <davidscheibe@Davids-MacBook-Pro.local>
2016-11-04 19:32:43 -0700

    trying to fix comment stream

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -22,7 +22,7 @@ class MicropostsController < ApplicationController
   private
 
     def micropost_params
-      params.require(:micropost).permit(:content, :picture)
+      params.require(:micropost).permit(:content,:receiver_id, :picture)
     end
     def correct_user
       @micropost = current_user.microposts.find_by(id: params[:id])
--- db/migrate/20160608164853_create_microposts.rb
+++ db/migrate/20160608164853_create_microposts.rb
@@ -2,6 +2,7 @@ class CreateMicroposts < ActiveRecord::Migration[5.0]
   def change
     create_table :microposts do |t|
       t.text :content
+      t.integer :receiver_id
       t.references :user, foreign_key: true
 
       t.timestamps
