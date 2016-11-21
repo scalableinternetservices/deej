7bb617d2a57168ffe4a316f202b32d2ac631377d
David scheibe <davidscheibe@wifi-131-179-32-87.host.ucla.edu>
2016-11-21 15:27:13 -0800

    added server side caching for various lists/results

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -1,2 +1,5 @@
 module MicropostsHelper
+	def cache_key_for_comments(comments)
+    	"comments-#{comments}-#{comments.count}"
+	end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,4 +7,24 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+
+  	def cache_key_for_topdjs(djs)
+    	"djs-#{djs.count}-#{User.maximum(:updated_at)}"
+	end
+
+	def cache_key_for_user_info(user)
+		"user-#{user.updated_at}"
+	end
+
+	def cache_key_for_songs(user)
+		"songs-#{user.songs.size}-#{user.song_ids}"
+	end
+
+	def cache_key_for_followers(user)
+		"followers-#{user.followers.count}-#{Relationship.where("followed_id = :user_id", user_id: user.id)}"
+	end
+
+	def cache_key_for_following(user)
+		"following-#{user.followers.count}-#{Relationship.where("follower_id = :user_id", user_id: user.id)}"
+	end
 end
